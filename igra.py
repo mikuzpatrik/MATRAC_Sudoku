594e6516815aaace714f38b565fa74ffea08391e
mikuzpatrik <patrik.mikuz@gmail.com>
2024-03-04 22:19:03 +0100

    bla bla
    commit hehe

--- igra.py
+++ igra.py
@@ -6,48 +6,14 @@ import copy
 
 def gen_mapping():
     indeks_to_obmocje_map = {}
+    obmocje_to_indeks_map = {i: [] for i in range(1, 10)}
     for i in range(9):
         for j in range(9): 
-            if i < 3:
-                if j < 3:
-                    obmocje = 1
-                elif j < 6:
-                    obmocje = 4
-                elif j < 9: 
-                    obmocje = 7
-            elif i < 6:
-                if j < 3:
-                    obmocje = 2
-                elif j < 6:
-                    obmocje = 5
-                elif j < 9: 
-                    obmocje = 8
-            elif i < 9:
-                if j < 3:
-                    obmocje = 3
-                elif j < 6:
-                    obmocje = 6
-                elif j < 9: 
-                    obmocje = 9
-            indeks_to_obmocje_map[str(i) + " " + str(j)] = obmocje 
-    return indeks_to_obmocje_map 
-
-def gen_mapping_2():
-    indeks_to_obmocje_map = {}
-    for i in range(4):
-        for j in range(4): 
-            if i < 2:
-                if j < 2:
-                    obmocje = 1
-                elif j < 4:
-                    obmocje = 3
-            elif i < 4:
-                if j < 2:
-                    obmocje = 2
-                elif j < 4:
-                    obmocje = 4
-            indeks_to_obmocje_map[str(i) + " " + str(j)] = obmocje 
-    return indeks_to_obmocje_map 
+            obmocje = 3 * (i // 3) + j // 3 + 1 
+            key = str(i) + " " + str(j)
+            indeks_to_obmocje_map[key] = obmocje
+            obmocje_to_indeks_map[obmocje] = obmocje_to_indeks_map[obmocje] + [key]
+    return indeks_to_obmocje_map, obmocje_to_indeks_map 
 
 def gen_grid(grid_size):
     vrstice = [[i for i in range(1, grid_size + 1)] for j in range(grid_size)]
@@ -65,7 +31,7 @@ def gen_grid(grid_size):
     elif grid_size == 9: 
         grid = [[None for i in range(grid_size)] for j in range(grid_size)]
         obmocja = [[] for k in range(grid_size)]
-        mapping = gen_mapping()
+        mapping, mapping2 = gen_mapping()
         for i in range(grid_size):
             for j in range(grid_size):
                 obmocje = mapping[str(i) + " " + str(j)]
@@ -89,20 +55,84 @@ def get_game(grid_size, missing_cells):
         except:
             pass
 
-    print(np.array(grid))
     game = copy.deepcopy(grid)
-    mapping = gen_mapping()
+    mapping, mapping2 = gen_mapping()
     inds = list(mapping.keys())
     inds = random.sample(inds, missing_cells)
     for el in inds: 
         i = int(el[0])
         j = int(el[-1])
         game[i][j] = None
-    return grid, game
+    return np.array(grid), np.array(game)
 
 def plot_game(grid, game):
     print(tabulate(grid, tablefmt="rounded_grid"))
     print(tabulate(game, tablefmt="rounded_grid"))
+
+
+def try_num(game, num, i, j):
+    allowed = True
+    if game[i][j] is not None:
+        allowed = False
+    else:
+        if num in game[i]:
+            allowed = False
+        elif num in np.transpose(game)[j]:
+            allowed = False
+        else:
+            mapping, mapping2 = gen_mapping()
+            obmocje = mapping[str(i) + " " + str(j)]
+            for el in mapping2[obmocje]:
+                if game[int(el[0])][int(el[-1])] == num:
+                    allowed = False
+    return allowed
+
+def count_input_options(game): 
+    counter = {}
+    for i in range(9):
+        for j in range(9):
+            if game[i][j] is None:
+                counter[str(i) + " " + str(j)] = 0
+                for num in range(1, 10):
+                    if try_num(game, num, i, j):
+                        counter[str(i) + " " + str(j)] += 1
+    print(counter)
+
+def check_game(game): 
+    if any(None in sub for sub in game):
+        print("Igra ni koncana!")
+        return False 
+    else:
+        for i in range(9):
+            for j in range(9):
+                num = game[i][j]
+                if game[i].count(num) == 1:
+                    vrstica_check = True 
+                if np.transpose(game)[j].count(num) == 1:
+                    stolpec_check = True 
+            mapping, mapping2 = gen_mapping()
+            obmocje = mapping[str(i) + " " + str(j)]
+            num_count = 0
+            for el in mapping2[obmocje]:
+                if game[int(el[0])][int(el[-1])] == num:
+                    num_count += 1
+            if num_count == 1:
+                obmocje_check = True 
+        if vrstica_check and stolpec_check and obmocje_check:
+            print("Igra je pravilno resena")
+            return True
+        else:
+            print("Igra ni pravilno resena")
+            return False 
+
+# random.seed(0)
+
+# grid, game = get_game(9, 40)
+# plot_game(grid, game)
+
+# count_input_options(game)
+
+# check_game(game)
 
 # Solver idea: 
     # generiram igro 
